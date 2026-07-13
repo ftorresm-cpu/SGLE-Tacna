812f94c1b676e228fcfaee8d1b0e1689891ca9ac
zAlonso67 <alonsgamerxd@gmail.com>
2026-07-13 00:50:40 -0500

    Alonso - Opción 1: Implementación de la función registrarLlamada

--- proyecto/emergencia.cpp
+++ proyecto/emergencia.cpp
@@ -1,1 +1,24 @@
 #include <iostream>
+#include <string>
+#include "emergencia.h"
+using namespace std;
+void registrarLlamada(Emergencia lista[], int &contador) {
+    if (contador >= 100) {
+        cout << "\n[!] Almacenamiento lleno.\n";
+        return;
+    }
+    cout << "\n--- REGISTRAR EMERGENCIA ---\n";
+    lista[contador].idLlamada = 1000 + contador + 1;
+    cout << "Nombre del ciudadano (Sin_espacios): ";
+    cin >> lista[contador].nombre;
+    cout << "Zona o Distrito de Tacna: ";
+    cin >> lista[contador].zona;
+    cout << "Institucion (Policia/Bomberos/Serenazgo): ";
+    cin >> lista[contador].institucion;
+    cout << "Hora del suceso (HH:MM): ";
+    cin >> lista[contador].hora;
+    
+    lista[contador].estado = "Pendiente";
+    contador++;
+    cout << "\n[OK] Emergencia guardada con ID: " << lista[contador-1].idLlamada << endl;
+}

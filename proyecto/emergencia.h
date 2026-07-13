84298addf74c9eaec8ba47ffde41ce9f2a1bc5a1
Giovannym27 <gimamanim@unjbg.edu.pe>
2026-07-13 00:47:13 -0500

    Giovanny - Commit 2: Inclusion de prototipos modulares para operaciones CRUD

--- proyecto/emergencia.h
+++ proyecto/emergencia.h
@@ -12,4 +12,10 @@ struct Emergencia {
     string estado;
 };
 
+void registrarLlamada(Emergencia lista[], int &contador);
+void mostrarLlamadas(const Emergencia lista[], int contador);
+void buscarEmergencia(const Emergencia lista[], int contador);
+void modificarEstado(Emergencia lista[], int contador);
+void eliminarLlamada(Emergencia lista[], int &contador);
+
 #endif

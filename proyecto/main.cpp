cf5f963400312619368055f47eaa035d6b304c8f
ftorresm-cpu <ftorresm@unjbg.edu.pe>
2026-07-13 00:26:00 -0500

    Líder - Commit 2: Implementación y conexión total con funciones

--- proyecto/main.cpp
+++ proyecto/main.cpp
@@ -13,22 +13,38 @@ int main() {
         cout << "\n==================================================\n";
         cout << "   CENTRAL CENTRALIZADA DE EMERGENCIAS - TACNA\n";
         cout << "==================================================\n";
-        cout << "1. Registrar Llamada\n";
-        cout << "2. Listar Emergencias\n";
-        cout << "3. Buscar por ID\n";
-        cout << "4. Modificar Estado\n";
-        cout << "5. Eliminar Registro\n";
+        cout << "1. Registrar Llamada de Emergencia (Create)\n";
+        cout << "2. Listar Todas las Emergencias (Read)\n";
+        cout << "3. Buscar Emergencia por ID (Read)\n";
+        cout << "4. Modificar Estado de la Emergencia (Update)\n";
+        cout << "5. Eliminar Registro de Emergencia (Delete)\n";
         cout << "6. Salir del Sistema\n";
         cout << "==================================================\n";
         cout << "Seleccione una opcion (1-6): ";
         cin >> opcion;
 
-        if (opcion == 6) {
-            cout << "\nCerrando el sistema correctamente...\n";
-        } else if (opcion < 1 || opcion > 6) {
-            cout << "\n[ERROR] Opcion no valida.\n";
+        switch(opcion) {
+            case 1:
+                registrarLlamada(registros, totalRegistros);
+                break;
+            case 2:
+                mostrarLlamadas(registros, totalRegistros);
+                break;
+            case 3:
+                buscarEmergencia(registros, totalRegistros);
+                break;
+            case 4:
+                modificarEstado(registros, totalRegistros);
+                break;
+            case 5:
+                eliminarLlamada(registros, totalRegistros);
+                break;
+            case 6:
+                cout << "\nFinalizando monitoreo. Sistema cerrado correctamente.\n";
+                break;
+            default:
+                cout << "\n[ERROR] Opcion no valida. Intente de nuevo.\n";
         }
-
     } while(opcion != 6);
 
     return 0;
